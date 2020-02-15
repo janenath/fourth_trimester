964af851600f4f5b7f4aca699a66858195d38230
Jane Nath <jane.e.nath@gmail.com>
2020-02-15 08:32:10 -0800

    initialize rails backend and article, tip models

---
+++ fourth_trimester_api/db/migrate/20200215162825_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.integer :week
+      t.string :content
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
