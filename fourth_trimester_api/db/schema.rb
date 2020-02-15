ff17e594cafc0e31cea400ad55f0554c52bb27e6
Jane Nath <jane.e.nath@gmail.com>
2020-02-15 10:07:59 -0800

    auth working on backend

--- fourth_trimester_api/db/schema.rb
+++ fourth_trimester_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_173636) do
+ActiveRecord::Schema.define(version: 2020_02_15_175714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_02_15_173636) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.date "due_date"
     t.json "articles"
     t.json "scores"
--- fourth_trimester_api/db/seeds.rb
+++ fourth_trimester_api/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+articles = Article.create([
+    {
+        title: "hydration",
+        content: "hydration is really important",
+        week: "1",
+        image: "https://images.unsplash.com/photo-1539805015143-9f78208ec73f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80"
+    }
+])
