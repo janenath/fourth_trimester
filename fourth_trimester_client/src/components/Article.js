d593e1aa28960d9dbb045ad74ce57137a0df8c39
Jane Nath <jane.e.nath@gmail.com>
2020-02-15 10:22:04 -0800

    initialize create-react-app frontend

---
+++ fourth_trimester_client/src/components/Article.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Article extends Component {
+    render() {
+        return (
+            <div className="ArticleContainer">
+                <h2>Title</h2>
+                <p>Content</p>
+            </div> 
+        )
+    }
+}
+
+export default Article;
