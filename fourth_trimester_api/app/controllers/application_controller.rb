472fc8ec101f90b011c40f84e603037b1893d0d2
Jane <jane.e.nath@gmail.com>
2020-02-26 09:42:10 -0800

    merged article and user changes

--- fourth_trimester_api/app/controllers/application_controller.rb
+++ fourth_trimester_api/app/controllers/application_controller.rb
@@ -1,19 +1,32 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
-    helper_method :current_user
-    helper_method :logged_in?
+  before_action :authenticate_request
+  attr_reader :current_user
 
-    def current_user
-      User.find_by(id: session[:user_id])
-    end
+  private
 
-    def logged_in?
-      !current_user.nil?
-    end
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
+end
 
-    def authorized
-      redirect_to '/home' unless logged_in?
-    end
+
+# class ApplicationController < ActionController::API
+    # before_action :authorized
+    # helper_method :current_user
+    # helper_method :logged_in?
+
+    # def current_user
+    #   User.find_by(id: session[:user_id])
+    # end
+
+    # def logged_in?
+    #   !current_user.nil?
+    # end
+
+    # def authorized
+    #   redirect_to '/home' unless logged_in?
+    # end
     # attr_reader :current_user
   
     # private
@@ -22,4 +35,4 @@ class ApplicationController < ActionController::API
     #   @current_user = AuthorizeApiRequest.call(request.headers).result
     #   render json: { error: 'Not Authorized' }, status: 401 unless @current_user
     # end
-end
+# end
